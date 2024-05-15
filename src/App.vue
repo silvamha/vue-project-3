4f6575fd6c681201ded008be8a36b221d99a8d54
Ed <silva.email@yahoo.com>
2024-05-15 17:37:38 -0500

    refactor: Remove unnecessary code and update index.html
    - Remove the <h1>Hello</h1> element from index.html
    - Update index.html with meta charset and viewport tags
    
    The <h1>Hello</h1> element was removed from index.html as it was no longer needed. Additionally, the meta charset and viewport tags were added to improve the page's accessibility and responsiveness.

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,10 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import Greeting from './components/Greeting.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-    <Greeting />
-  </main>
+<h1>Hello World!</h1>
 </template>
 
-<!-- <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<script>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style> -->
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
+// import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+let vm = createApp(App);
+
+vm.mount("#app");
