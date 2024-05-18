26a4b022e2abe027e3b59d69b81537d7281ed78c
Ed <silva.email@yahoo.com>
2024-05-17 21:27:37 -0500

    refactor: Add name prop to Greeting.vue component
    The Greeting.vue component has been updated to include a new prop called "name". This change allows for dynamic rendering of the user's name in the greeting message, enhancing personalization and customization.

--- src/components/Greeting.vue
+++ src/components/Greeting.vue
@@ -2,6 +2,7 @@
     <div>
         <h2 v-if="age > 25">{{ msg }}</h2>
         <p v-else> You must be 25 years or older to view this message.</p>
+        <p>{{ name }}</p>
     </div>
 </template>
     <script>
